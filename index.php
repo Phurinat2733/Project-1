7b345a857cd56c66099dfaf8102d807552e4b020
Phurinat2733 <42757420+Phurinat2733@users.noreply.github.com>
2018-10-06 11:30:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -246,7 +246,7 @@ if (!is_null($events['events']))
 			{
 				$messages = [
 				'type' => 'video',
-				'originalContentUrl' => "https://www.dropbox.com/s/93foh9yitu5mb65/Ed%20Sheeran%20-%20Shape%20of%20You%20%5BOfficial%20Video%5D.mp4?dl=0",
+				'originalContentUrl' => "https://www.dropbox.com/s/93foh9yitu5mb65/Ed%20Sheeran%20-%20Shape%20of%20You%20%5BOfficial%20Video%5D.mp4",
     				'previewImageUrl' => "https://www.picz.in.th/images/2018/10/06/hwH99D.jpg"];
 			}
 			
